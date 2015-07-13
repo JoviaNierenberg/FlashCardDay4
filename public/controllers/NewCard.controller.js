9e72f60c7aaf534f12b0e3b4b72bea797dbf7f5e
Jovia Nierenberg <nierenberg.jovia@gmail.com>
2015-07-13 17:33:34 -0400

    added new card form functionality

--- public/controllers/NewCard.controller.js
+++ public/controllers/NewCard.controller.js
@@ -10,13 +10,46 @@ app.controller('NewCardController', function ($scope, NewCardFactory, FlashCardF
 	}
 
 	$scope.doPostRequest = function (card) {
+		if (card.question === null){
+			console.log("there was no question")
+		}
 		return NewCardFactory.request(card)
 		.then(function(card) {
 		      FlashCardFactory.flashCards.push(card);
+		      // $scope.newCardForm.$setPristine()
+		      // card.question = null
+		      // card.answers.forEach(function(answer){
+		      // 	answer.text = null
+		      // 	answer.correct = false
+		      // })
 		})
 	}
-
+	$scope.showButton = function (card){
+		var answerTooLong = false
+		card.answers.forEach(function(answer){
+			if (answer.text !== null && answer.text.length > 40) {
+				answerTooLong = true
+				console.log("answer is too long")
+			}	
+		})
+		if (card.question !== null && card.question.length >= 15 && card.category !== null && !answerTooLong){
+			return true
+		}
+		return false
+	}
+	$scope.reset = function(){
+		$scope.newCard = {
+		    question: null,
+		    category: null,
+		    answers: [
+		        { text: null, correct: false },
+		        { text: null, correct: false },
+		        { text: null, correct: false }
+		    ]
+		}
+	}
 })
+
 
 
 
