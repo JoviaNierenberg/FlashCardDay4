b2109203caa39dfa9a3f8deb19439926b5273305
celanajaya <petermsteele@gmail.com>
2015-07-14 15:52:21 -0400

    ui-router with three states

--- public/app.js
+++ public/app.js
@@ -1,1 +1,20 @@
-var app = angular.module('FlashCardApp', []);
+var app = angular.module('FlashCardApp', ['ui.router'])
+
+.config(function($stateProvider){
+		$stateProvider
+			.state('flashCardsView', {
+				url: "/",
+				controller: 'FlashCardCtrl',
+				templateUrl: 'templates/flashcardsview.html'
+			})
+			.state('flashCardsForm', {
+				url: "/form",
+				controller: 'NewCardController',
+				templateUrl:'templates/flashcardsform.html'
+			})
+			.state('statistics', {
+				url: "/stats",
+				controller: 'StatsController', 
+				templateUrl: 'templates/stats.html'
+			});
+	})
